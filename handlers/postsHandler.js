c7c20f81455bbf4d5b03787c06854a0eb3bc4201
. <barcalover016@gmail.com>
2024-10-29 23:26:11 +0100

    completing create and delete functions

--- handlers/postsHandler.js
+++ handlers/postsHandler.js
@@ -24,7 +24,6 @@ export const getPostByID = async (req, res) => {
 
 export const createPost = async (req, res) => {
   const { title, content } = req.body;
-
   if (!title || !content) {
     return res.status(400).json({ error: "Title and content are requireds" });
   }
@@ -38,8 +37,48 @@ export const createPost = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
-  console.log("update one");
+  const { id } = req.params;
+  const { title, content } = req.body;
+  if (!title && !content) {
+    return res.status(400).json({
+      error: "At least one field (title or content) is required to update",
+    });
+  }
+  const updatedFields = [];
+  const values = [];
+  if (title) {
+    updatedFields.push("title = ?");
+    values.push(title);
+  }
+  if (content) {
+    updatedFields.push("content = ?");
+    values.push(content);
+  }
+  values.push(id);
+
+  try {
+    await db.query(
+      `UPDATE posts SET ${updatedFields.join(", ")} WHERE id = ?`,
+      values
+    );
+    return res.status(200).json({ success: "Post updated successfully" });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: "An error occurred while updating the post" });
+  }
 };
 export const deletePost = async (req, res) => {
-  console.log("delete one");
+  const { id } = req.params;
+  try {
+    const result = await db.query("DELETE FROM posts WHERE id = ?", [id]);
+    if (result.effectedRows === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    return res.status(200).json({ success: "Post deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "An error occurred while deleting the post" });
+  }
 };
